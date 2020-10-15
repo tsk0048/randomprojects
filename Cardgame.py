2b4376095a57f9d0a9f6a7fbd1c1b47abd64756a
tsk0048 <khantashfan322@gmail.com>
2020-10-15 09:22:11 -0500

    Shuffle is slightly fixed new rounds do not generate new cards, but now there is no more list problem

--- Cardgame.py
+++ Cardgame.py
@@ -1,4 +1,4 @@
-from random import shuffle
+import random
 from time import sleep
 
 # 21 game. It takes 1 player and 1 dealer. they each get 3 cards. the sum of the 3 cards is checked to see =21
@@ -7,17 +7,13 @@ from time import sleep
 # Maybe try implementing a chip system. that could be cool
 
 # Deck. Numeric only, for now.
-cards = []
-for i in range(2, 14):
-    shuffle(cards)
-    cards.append(i)
-
+# This branch will try out randint instead of actual cards
+p_card1 = random.randint(2, 14)
+p_card2 = random.randint(2, 14)
+p_card3 = random.randint(2, 14)
 
 # Player hand. Contains the cards as well as unique actions for the player.
 def player_hand():
-    p_card1 = cards[0]
-    p_card2 = cards[1]
-    p_card3 = cards[2]
     print('For this round, your cards are: {}, {}, {} .'.format(p_card1, p_card2, p_card3))
     sum1 = p_card1 + p_card2 + p_card3
     print("The sum of your cards is " + str(sum1) + '.')
@@ -25,19 +21,20 @@ def player_hand():
 
 # Initial sum of cards
 def initial_sum():
-    return cards[0] + cards[1] + cards[2]
+    return p_card1 + p_card2 + p_card3
 
 # Action is unique to players.
 def p_hit():
-    return cards[10] + cards[0] + cards[1] + cards[2]
+    return p_card1 + p_card2 + p_card3
 
 
 
 # Dealer hand
+d_card1 = random.randint(2, 14)
+d_card2 = random.randint(2, 14)
+d_card3 = random.randint(2, 14)
+
 def dealer_hand():
-    d_card1 = cards[3]
-    d_card2 = cards[4]
-    d_card3 = cards[5]
     print('For this round, the dealer had: {}, {}, {} '.format(d_card1, d_card2, d_card3))
     card_sum = d_card1 + d_card2 + d_card3
     print("The dealer had " + str(card_sum) + " in total.")
@@ -64,7 +61,7 @@ def game():
             sleep(2.5)
             print('You win!')
             dealer_hand()
-            if input('Would you like to play again?') in ('Yes' , 'y' , 'yes'):
+            if input('Would you like to play again?') in ('Yes', 'y', 'yes'):
                 return game()
             else:
                 break
@@ -75,7 +72,7 @@ def game():
             a = input("Would you like to hit or fold?")
 
             if a == 'hit':
-                print('The new card is: ' + str(cards[10]))
+                print('The new card is: ' + str(random.randint(2, 14)))
                 print('The new sum is: ' + str(p_hit()))
                 sleep(2.5)
                 if p_hit() > 21:
